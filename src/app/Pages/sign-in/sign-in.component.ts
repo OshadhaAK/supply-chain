d4516d5825e128d52e47cd8a4e5532fd94088560
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2018-11-03 00:18:22 +0530

    git push

--- src/app/Pages/sign-in/sign-in.component.ts
+++ src/app/Pages/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestsServiceService} from '../../services/requests-service.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,13 +12,30 @@ export class SignInComponent implements OnInit {
   username: string;
   password: string;
 
-  constructor() { }
+  constructor(private reqService:RequestsServiceService,
+    private messages:FlashMessagesService) { }
 
   ngOnInit() {
   }
 
   onLoginSubmit() {
     console.log("submitted");
+    const user={
+      username:this.username,
+      password:this.password
+    };
+    this.reqService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.messages.show(data.msg, {
+          cssClass: 'alert-success',
+          timeOut: 5000 });
+
+      } else {
+        this.messages.show(data.msg, {
+          cssClass: 'alert-danger',
+          timeOut: 5000 });
+      }
+    });
   }
 
 
